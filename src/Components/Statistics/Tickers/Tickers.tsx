cc58aa01a880f151f94696ca91b042aef9aaf9d8
Oleg Klyzki <olegausss368@gmail.com>
2023-08-14 16:48:55 +0300

    component-ticker

--- src/Components/Statistics/Tickers/Tickers.tsx
+++ src/Components/Statistics/Tickers/Tickers.tsx
@@ -10,9 +10,7 @@ import Table from '@mui/material/Table';
 import tickers from '../../../DataFiles/tickers.json'
 import TableCell from '@mui/material/TableCell';
 import TablePagination from '@mui/material/TablePagination';
-import { Box } from '@mui/material';
-import { Ticker } from 'react-ts-tradingview-widgets-fixed';
-import { parse } from 'path';
+import { Box, Checkbox } from '@mui/material';
 
 export interface Ticker {
 	"symbol": string,
@@ -20,12 +18,14 @@ export interface Ticker {
 	"Last Price": string,
 	"Industry / Category": string,
 	"Type": string,
-	"Exchange": string
+	"Exchange": string,
+	index?: number
 }
 
 export interface Column {
 	id: 'symbol' | 'Name' | 'Last Price' | 'Industry / Category' | 'Type' | 'Exchange',
-	label: string
+	label: string,
+	index?: number
 }
 
 export enum ColumnType {
@@ -37,45 +37,47 @@ export enum ColumnType {
 	"Exchange" = "Exchange"
 }
 
-
-export const parseHeadData = () => {
-	const data: any = Object.values(tickers)
-	const headData = Object.keys(data[0])
-
-	const res: Column[] = headData.map((data) => {
-
-		const t: Column = {
-			id: data.toString() as ColumnType.symbol,
-			label: data
-		}
-		return t
-	})
-	return res
-};
-
-export const parseData = () => {
-	const data: Array<Ticker> = Object.values(tickers);
-	const tickersData: Array<Ticker> = data.map((ticker) => {
-		const res: Ticker = {
-			"symbol": ticker.symbol,
-			"Name": ticker.Name,
-			"Last Price": ticker['Last Price'],
-			"Industry / Category": ticker['Industry / Category'],
-			"Type": ticker.Type,
-			"Exchange": ticker.Exchange
-		}
-		return res;
-	})
-	
-	return tickersData
-
-};
-
 const Tickers = () => {
 	const [page, setPage] = React.useState(0);
-	const [rowsPerPage, setRowsPerPage] = React.useState(100);
+	const [rowsPerPage, setRowsPerPage] = React.useState(10);
 	const [data, setData] = useState('');
-	const [allTickers, setAllTickers] = useState<Array<Ticker>>(parseData());
+
+	const parseHeadData = () => {
+		const dataTickers: Ticker[] = Object.values(tickers);
+		const headData = Object.keys(dataTickers[0]);
+		const res: Column[] = headData.map((data) => {
+			const newData: Column = {
+				id: data.toString() as ColumnType.symbol,
+				label: data,
+				index: 0
+			}
+			return newData;
+		});
+		res.forEach((ticker, index) => ticker.index = index);
+		return res;
+	};
+
+	const parseData = (param: string) => {
+		const data: Array<Ticker> = Object.values(tickers);
+		const tickersData: Array<Ticker> = data.map((ticker) => {
+			const res: Ticker = {
+				"symbol": ticker.symbol,
+				"Name": ticker.Name,
+				"Last Price": ticker['Last Price'],
+				"Industry / Category": ticker['Industry / Category'],
+				"Type": ticker.Type,
+				"Exchange": ticker.Exchange,
+				index: 0
+			}
+			return res;
+		})
+		tickersData.forEach((ticker, index) => ticker.index = index);
+		if (param) {
+			return tickersData.filter((ticker) => ticker.symbol.toLowerCase().includes(param) ? ticker : undefined);
+		}
+		return tickersData;
+	};
+
 
 	const handleChangeData = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setData(event.target.value)
@@ -90,61 +92,35 @@ const Tickers = () => {
 		setPage(0);
 	};
 
-	const identifier = (data: string)  => {
-		data = data.toLowerCase();
-		const res = parseData().map((tickerSym) => {
-			const sym = tickerSym.symbol.toLowerCase();
-			
-			if (!sym.includes(data)) {
-				const res1: Ticker = {
-					symbol: '',
-					Name: '',
-					'Last Price': '',
-					'Industry / Category': '',
-					Type: '',
-					Exchange: ''
-				};
-				return res1;
-			}
-			
-			console.log(tickerSym);
-				return tickerSym
-		})
-		
-		setAllTickers(res);
-	};
-
-	
-
 	useEffect(() => {
-		data && identifier(data)
+
 	}, [data])
-	console.log()
+
 	return (
-		<Grid container >
-			<Box sx={{display: 'flex', flexDirection: 'column'}}>
-				<TextField variant="outlined" onChange={handleChangeData}/>
-				<TableContainer component={Paper} sx={{ width: '1500px' }}>
+		<Grid container sx={{ display: 'flex', justifyContent: 'center' }} >
+			<Box sx={{ display: 'flex', flexDirection: 'column' }}>
+				<TextField variant="outlined" onChange={handleChangeData} />
+				<TableContainer component={Paper} sx={{ width: '700px' }}>
 					<Table>
 						<TableHead>
 							<TableRow>
 								{parseHeadData().map((column: Column) => {
 									return (
-										<TableCell key={column.id}>{column.id}</TableCell>
+										<TableCell key={column.index}> {column.id}</TableCell>
 									);
 								})}
 							</TableRow>
 						</TableHead>
 						<TableBody>
-							{allTickers
+							{parseData(data)
 								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
 								.map((row) => {
 									return (
-										<TableRow key={+row.symbol}>
+										<TableRow key={row.index}>
 											{parseHeadData().map((column) => {
 												const value = row[column.id];
 												return (
-													<TableCell key={+column.id}>
+													<TableCell key={column.index}>
 														{value}
 													</TableCell>
 												)
@@ -157,9 +133,9 @@ const Tickers = () => {
 					</Table>
 				</TableContainer>
 				<TablePagination
-					rowsPerPageOptions={[100, 500, 1000]}
+					rowsPerPageOptions={[10, 100, 1000]}
 					component="div"
-					count={parseData().length}
+					count={parseData(data).length}
 					rowsPerPage={rowsPerPage}
 					page={page}
 					onPageChange={handleChangePage}
@@ -167,7 +143,7 @@ const Tickers = () => {
 				/>
 			</Box>
 
-		</Grid>
+		</Grid >
 	)
 }
 
