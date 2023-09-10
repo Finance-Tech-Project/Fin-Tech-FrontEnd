9551d5bffaffd28d7ecc0380272da5cd1fc242ca
OlegKlyzki <olegausss368@gmail.com>
2023-09-10 20:47:59 +0300

    migrateNewServerStart

--- src/Constants/fetchConstants.ts
+++ src/Constants/fetchConstants.ts
@@ -5,5 +5,8 @@ export const TICKER_DATA = "ticker/daily/symbol/";
 export const MAIN_DATA = "main_data";
 export const VOLUME_DATA = "volume_data";
 
-export const BASE_URL_AMAZON = "http://fin-tech.eu-north-1.elasticbeanstalk.com/";
-export const ALL_SYMBOLS = "allsymbols";
+export const BASE_URL_AMAZON = "http://fin-tech.eu-north-1.elasticbeanstalk.com";
+export const ALL_SYMBOLS = "/allsymbols";
+export const QUOTE_HISTORY = "/quote/history";
+
+// http://fin-tech.eu-north-1.elasticbeanstalk.com/quote/history?dateFrom=2021-09-10&dateTo=2023-09-10&ticker=AAPL
