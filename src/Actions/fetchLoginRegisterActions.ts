e29d0a9320107552766fb2d62cf7621a6921a9ea
OlegKlyzki <olegausss368@gmail.com>
2023-12-07 22:56:22 +0200

    migrate server

--- src/Actions/fetchLoginRegisterActions.ts
+++ src/Actions/fetchLoginRegisterActions.ts
@@ -8,16 +8,15 @@ import { AppDispatch } from "../app/store";
 export const registerUser = (user: UserRegister) => {
     return async (dispatch: AppDispatch) => {
         try {
-            const response = await fetch(`${
-                    FetchConstants.BASE_URL +
-                    FetchConstantsForLoginRegister.ACCOUNT +
-                    FetchConstantsForLoginRegister.REGISTER
+            const response = await fetch(`${FetchConstants.BASE_URL +
+                FetchConstantsForLoginRegister.ACCOUNT +
+                FetchConstantsForLoginRegister.REGISTER
                 }`, {
-                    method: 'POST',
-                    body: JSON.stringify(user),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             });
             if (response.ok) {
                 const data: UserProfile = await response.json();
@@ -33,22 +32,21 @@ export const registerUser = (user: UserRegister) => {
 export const loginUser = (token: string) => {
     return async (dispatch: AppDispatch) => {
         try {
-            const response = await fetch(`${
-                    FetchConstants.BASE_URL +
-                    FetchConstantsForLoginRegister.ACCOUNT +
-                    FetchConstantsForLoginRegister.LOGIN
+            const response = await fetch(`${FetchConstants.BASE_URL +
+                FetchConstantsForLoginRegister.ACCOUNT +
+                FetchConstantsForLoginRegister.LOGIN
                 }`, {
-                    method: 'POST',
-                    headers: {
-                        Authorization: token,
-                        'Content-Type': 'application/json'
-                    }
+                method: 'POST',
+                headers: {
+                    Authorization: token,
+                    'Content-Type': 'application/json',
+                }
             });
             if (response.status === 401) {
                 dispatch(putUser(null));
                 dispatch(putToken(null));
                 sessionStorage.clear();
-                throw new Error(response.status + '')
+                throw new Error('401')
                 // throw new TypeError("You not authorized or you not have account in this site. Please go in page sign up and create you account or if you have account go in page sign in and authorize in system.");  
             }
             if (response.ok) {
@@ -59,7 +57,10 @@ export const loginUser = (token: string) => {
             }
         } catch (error) {
             if (error instanceof Error) {
-               console.log(error.message); 
+                // dispatch(putUser(null));
+                // dispatch(putToken(null));
+                // sessionStorage.clear();
+                console.log(error);
             }
         }
     }
